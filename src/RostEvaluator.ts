be2db1bac42ae4e8f1d2cc25baa94601d1d655d4
danielstoopedahl <daniel.stoopedahl@gmail.com>
2025-04-10 19:19:06 +0800

    just testing

--- src/RostEvaluator.ts
+++ src/RostEvaluator.ts
@@ -144,13 +144,13 @@ class RostEvaluatorVisitor extends AbstractParseTreeVisitor<object> implements R
 
     }
 
-    visitExpressionStatement(ctx: ExpressionStatementContext): object{
-        return {
-            tag: "expr_stmt",
-            expr: ctx.expression()
-        }
+    // visitExpressionStatement(ctx: ExpressionStatementContext): object{
+    //     return {
+    //         tag: "expr_stmt",
+    //         expr: ctx.expression()
+    //     }
 
-    }
+    // }
     visitBlock(ctx: BlockContext): object{
         return {
             tag: "blk",
