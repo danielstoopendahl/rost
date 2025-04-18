67f5b965ea23f28c1a611268884c8684c603891b
danielstoopedahl <daniel.stoopedahl@gmail.com>
2025-04-18 13:48:51 +0800

    can now run locally

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import { initialise } from "conductor/dist/conductor/runner/util/";
 import { RostEvaluator } from "./RostEvaluator";
+import { evaluate } from "./LocalEvaluator";
 
-const {runnerPlugin, conduit} = initialise(RostEvaluator);
+//const {runnerPlugin, conduit} = initialise(RostEvaluator);
+
+const program = `
+fn f(a: i32) -> bool {
+    returnera 2;
+}
+`
+
+evaluate(program)
