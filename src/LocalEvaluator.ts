4b0d21a1754d3bf673bc31864bd43bdbb652e100
danielstoopendahl <daniel.stoopendahl@gmail.com>
2025-04-18 13:48:51 +0800

    can now run locally

--- src/LocalEvaluator.ts
+++ src/LocalEvaluator.ts
@@ -1,6 +1,3 @@
-import * as fs from 'fs';
-import { BasicEvaluator } from "conductor/dist/conductor/runner";
-import { IRunnerPlugin } from "conductor/dist/conductor/runner/types";
 import { CharStream, CommonTokenStream } from 'antlr4ng';
 import { RostLexer } from './parser/grammar/RostLexer';
 import { RostParser } from './parser/grammar/RostParser';
@@ -9,38 +6,35 @@ import { check_type } from './TypeChecker';
 import { RostJSONBuilder } from "./TreeBuilder";
 
 
-export class RostEvaluator{
-    private visitor: RostJSONBuilder = new RostJSONBuilder();
+export function evaluate(chunk: string) {
+    try {
+        const visitor: RostJSONBuilder = new RostJSONBuilder();
+        // Create the lexer and parser
+        //const chunk = fs.readFileSync(0, 'utf-8'); // 0 refers to stdin
 
-    evaluate() {
-        try {
-            // Create the lexer and parser
-            const chunk = fs.readFileSync(0, 'utf-8'); // 0 refers to stdin
+        const inputStream = CharStream.fromString(chunk);
+        const lexer = new RostLexer(inputStream);
+        const tokenStream = new CommonTokenStream(lexer);
+        const parser = new RostParser(tokenStream);
+        // Parse the input
+        const tree = parser.prog();
+        
+        // Create JSON parse tree
+        const json_program = visitor.visit(tree);
+        console.log(`JSON tree: ${JSON.stringify(json_program)}`);
 
-            const inputStream = CharStream.fromString(chunk);
-            const lexer = new RostLexer(inputStream);
-            const tokenStream = new CommonTokenStream(lexer);
-            const parser = new RostParser(tokenStream);
-            // Parse the input
-            const tree = parser.prog();
-            
-            // Create JSON parse tree
-            const json_program = this.visitor.visit(tree);
-            console.log(`JSON tree: ${JSON.stringify(json_program)}`);
+        const program_type = check_type(json_program)
+        console.log(`Program type: ${program_type}`)
+        
+        console.log(`${go(json_program, 400)}`);
 
-            const program_type = check_type(json_program)
-            console.log(`Program type: ${program_type}`)
-            
-            console.log(`${go(json_program, 400)}`);
-
-            
-        }  catch (error) {
-            // Handle errors and send them to the REPL
-            if (error instanceof Error) {
-                console.log(`Error: ${error.message}`);
-            } else {
-                console.log(`Error: ${String(error)}`);
-            }
+        
+    }  catch (error) {
+        // Handle errors and send them to the REPL
+        if (error instanceof Error) {
+            console.log(`Error: ${error.message}`);
+        } else {
+            console.log(`Error: ${String(error)}`);
         }
     }
 }
